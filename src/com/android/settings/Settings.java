d94fb5e9901e1b12259b76a257cf061a156c339e
maxwen <maxwen@users.noreply.github.com>
2021-10-24 07:59:56 +0000

    Settings: Build BlazeHouse into Settings
    Signed-off-by: Sagarrokade006 <sagarrokade006@gmail.com>
    Signed-off-by: Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -325,4 +325,7 @@ public class Settings extends SettingsActivity {
      * Activity for AppDashboard.
      */
     public static class AppDashboardActivity extends SettingsActivity {}
+
+    // BlazeHouse Addition
+    public static class BlazeHouseActivity extends SettingsActivity {}
 }
